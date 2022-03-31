2b8a7738a2095c1ac1e965b9d3ae8379cc5b4cd8
kchesl <102710433+kchesl@users.noreply.github.com>
2022-03-30 23:14:03 -0400

    Update ProjectRunner.java

--- ProjectRunner.java
+++ ProjectRunner.java
@@ -1,19 +1,8 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Kirsten Chesley (kchesley888)
-package spacecolonies;
 
 import java.io.FileNotFoundException;
 import bsh.ParseException;
 
-/**
- *
- * @author Kirsten Chesley (kchesley888)
- * @version (2020-Apr-20)
- */
+
 public class ProjectRunner {
 
     /**
